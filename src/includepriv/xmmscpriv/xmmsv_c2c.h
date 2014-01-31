xmms2/xmms2-alumni

FEATURE(1598): Helper functions for c2c messages

src/includepriv/xmmscpriv/xmmsv_c2c.h
@@ -1,0 +1,25 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#ifndef __XMMS_PRIV_C2C_H__
+#define __XMMS_PRIV_C2C_H__
+
+#include "xmmsc/xmmsv_c2c.h"
+#include "xmmsc/xmmsc_stdint.h"
+
+xmmsv_t *xmmsv_c2c_message_format (int32_t sender, int32_t dest, int32_t id, xmmsv_t *payload);
+
+#endif

dsvensson/xmms2-devel

src/includepriv/xmmscpriv/xmmsv_c2c.h
@@ -1,0 +1,25 @@
+/*  XMMS2 - X Music Multiplexer System
+ *  Copyright (C) 2003-2011 XMMS2 Team
+ *
+ *  PLUGINS ARE NOT CONSIDERED TO BE DERIVED WORK !!!
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+#ifndef __XMMS_PRIV_C2C_H__
+#define __XMMS_PRIV_C2C_H__
+
+#include "xmmsc/xmmsv_c2c.h"
+#include "xmmsc/xmmsc_stdint.h"
+
+xmmsv_t *xmmsv_c2c_message_format (int64_t sender, int64_t dest, int64_t id, xmmsv_t *payload);
+
+#endif